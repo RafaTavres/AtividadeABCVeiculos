9b10c3f0ed39a16f1070f536fcb708dab966f883
Rafas <ragbriros@gmail.com>
2025-04-17 19:32:50 -0300

    base do projeto

---
+++ ABCVeiculosLib/Veiculo.cs
@@ -0,0 +1,27 @@
+public abstract class Veiculo
+{
+    public int Id { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public decimal Preco { get; set; }
+    public string Cor { get; set; }
+    public bool Vendido { get; protected set; }
+    public int IdadeVeiculo => DateTime.Now.Year - Ano;
+    public abstract string TipoVeiculo { get; }
+    public override string ToString()
+    {
+        return $"{Marca}, {Modelo}";
+    }
+
+    public Veiculo(int id, string marca, string modelo, int ano, decimal preco, string cor)
+    {
+        Id = id;
+        Marca = marca;
+        Modelo = modelo;
+        Ano = ano;
+        Preco = preco;
+        Cor = cor;
+        Vendido = false;
+    }
+}
---
+++ AplicacaoABCVeiculos/Program.cs
@@ -0,0 +1,12 @@
+﻿using ABCVeiculosLib;
+internal class Program
+{
+    Program()
+    {
+        
+    }
+    private static void Main(string[] args)
+    {
+        _ = new Program();
+    }
+}
